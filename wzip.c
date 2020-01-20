46bef8b839b08e779de105b1bd3fe4a842566397
Andreas <tug25511@temple.edu>
2020-01-20 18:11:24 -0500

    added fwrite functionality to the loop body, further testing required

--- wzip.c
+++ wzip.c
@@ -2,28 +2,25 @@
 Project 0
 Andreas Gagas
 
-wzip.c: .. ADD 
+wzip.c: .. "compress" the contents of the specified file using run-length encoding ... aaaaaaaaaabbbb --> 10a4b 
 */
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>  
-#define BUFFER_SIZE 256 
 
 int main(int argc, char *argv[]){
   
-  // aaaaaaaaaabbbb --> 10a4b 
-
-    if(argc == 1){
+  if(argc == 1){
     // no file specified on command line 
     
-    return 0;
+    puts("wzip: file1[file2...]");
+    exit(1);
   }
 
   for(int i=1; i<argc; ++i){
 
     FILE *fp = NULL; 
-    fp = fopen(argv[i], "r");
+    fp = fopen(argv[i], "wb"); // FIX "wb" mode ?? 
 
     if(fp == NULL){
       
@@ -33,44 +30,58 @@ int main(int argc, char *argv[]){
 
     unsigned int count = 1;
     int lastChar; // can cast to (char) to get its character 
-    int currentChar = 0;
+    int currentChar; 
 
     // grab each char, if next char is equal, increment count 
-    // otherwise use fwrite() to write the count and character
+    // otherwise use fwrite() to write the count and character to STDOUT
     // reset and repeat until currentChar returns EOF 
 
     lastChar = fgetc(fp); 
     
-    while(1){ // fix control 
+    while(1){  
 
       currentChar = fgetc(fp); 
 
       if(currentChar == EOF){
-        // display lastChar and its counts ADD 
-        break; 
+        // display count and lastChar for final chracter in the file
+
+        int retvalCount = fwrite(&count, sizeof(int), 1, stdout); 
+        int retvalChar = fwrite(&lastChar, sizeof(char), 1, stdout); 
+
+        if(retvalCount != 1 || retvalChar != 1)
+          puts("fwrite error");
+
+        //printf("TEST: count: %d\n character: %c\n", count, (char)lastChar); // test in "r" mode 
+
+        break; // exit loop 
       }
       
       if(currentChar == lastChar)
         ++count; 
-      
+
       else{
-        // fwrite the count
-        // fwrite the character
+
+        int retvalCount = fwrite(&count, sizeof(int), 1, stdout); // fwrite the count  
+        int retvalChar = fwrite(&lastChar, sizeof(char), 1, stdout); // fwrite the character
+
+        if(retvalCount != 1 || retvalChar != 1)
+          puts("fwrite error");
  
-        //currentChar = lastChar;
-        count = 0; 
+        //printf("TEST: count: %d\n character: %c\n", count, (char)lastChar); // test in "r" mode
+
+        count = 1; 
       }
 
       lastChar = currentChar; 
 
     }
 
+    int status = fclose(fp); // close file ptr
 
-    // close file 
+    if(status == EOF){
+      puts("error: failed to close file");
+    }
   }
-
-
-
 
   return 0; 
 }
