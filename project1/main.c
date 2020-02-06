59f2ed27243dd443a24493d7474d2dd59b42a3c5
Andreas <tug25511@temple.edu>
2020-02-05 20:22:45 -0500

    event handlers progress

--- project1/main.c
+++ project1/main.c
@@ -12,18 +12,28 @@ main.c - discrete event simulator
 // functions for priortiy queue: pushPQ(Queue *queue, struct event *e), struct event* popPQ(Queue *queue, struct event *e)
 
 //void readConfigFile(); // const values based on thoses read from the configuration file 
-int randomNumBetweenInterval(int min, int max);
-void startSimulation(int INIT_TIME, int *clock, Queue *pQ); 
-//void systemArrival(int ARRIVE_MIN, int ARRIVE_MAX, int *clock, int *counter, Queue *pQ); 
-//void endSimulation(int FIN_TIME, int *clock, Queue *pQ);
+int random_num_between_interval(int min, int max);
 
+void handle_start_simulation(Queue *pQ); 
+void handle_end_simulation(Queue *pQ);
+void handle_process_arrival(Queue *pQ, Queue *cpuQueue, struct event *old); 
+void hand_process_arrive_cpu(Queue *pQ, struct event *old);
+
+ // global variables (for simplicity)
+int SEED, INIT_TIME, FIN_TIME, ARRIVE_MIN, ARRIVE_MAX, CPU_MIN, CPU_MAX,
+DISK1_MIN, DISK1_MAX, DISK2_MIN, DISK2_MAX, NETWORK_MIN, NETWORK_MAX; 
+double QUIT_PROB, NETWORK_PROB; 
+
+int id_count; // testing this out 
 
 // 1 for T ex. SYSTEM_EXIT 0 --> FALSE
 
-// MOVE TO QUEUE.c            MOVE TO QUEUE.c   after completion event func. 
-
-// typedef enum {START_SIM, SYSTEM_ARRIVAL, CPU_START, CPU_END, SYSTEM_EXIT, DISK1_ARRIVAL, DISK1_START, DISK1_END, 
-//DISK2_ARRIVAL, DISK2_START, DISK2_END, NETWORK_ARRIVAL, NETWORK_START, NETWORK_END, END_SIM} TYPES;   
+// typedef enum {START_SIM, PROCESS_ARRIVAL, PROCESS_ARRIVE_CPU, PROCESS_EXIT_CPU, PROCESS_EXIT_SYSTEM, 
+// //                0             1                 2                  3                 4
+// PROCESS_ARRIVE_DISK1, PROCESS_EXIT_DISK1, PROCESS_ARRIVE_DISK2, PROCESS_EXIT_DISK2,
+// //      5                       6                 7                      8                        
+// PROCESS_ARRIVE_NETWORK, PROCESS_EXIT_NETWORK, END_SIM} TYPES;
+// //     9                        10                11
 
 // struct event{
 // // represent an event 
@@ -37,9 +47,9 @@ int main(int args, char *argv[]){
   
   // TEMP FIX FORMAT LATER 
   // const values (make const later), generate random num between their interval
-  int SEED, INIT_TIME, FIN_TIME, ARRIVE_MIN, ARRIVE_MAX, QUIT_PROB, NETWORK_PROB, 
-  CPU_MIN, CPU_MAX, DISK1_MIN, DISK1_MAX, DISK2_MIN, DISK2_MAX, 
-  NETWORK_MIN, NETWORK_MAX; 
+  // int SEED, INIT_TIME, FIN_TIME, ARRIVE_MIN, ARRIVE_MAX, CPU_MIN, CPU_MAX, 
+  // DISK1_MIN, DISK1_MAX, DISK2_MIN, DISK2_MAX,NETWORK_MIN, NETWORK_MAX; 
+  // double QUIT_PROB, NETWORK_PROB; 
 
   FILE *fptr = NULL;
   fptr = fopen("CONFIG1.conf", "r"); // change to command line input later 
@@ -53,8 +63,8 @@ int main(int args, char *argv[]){
   fscanf(fptr, "%*s %d", &FIN_TIME);
   fscanf(fptr, "%*s %d", &ARRIVE_MIN);
   fscanf(fptr, "%*s %d", &ARRIVE_MAX);
-  fscanf(fptr, "%*s %d", &QUIT_PROB);
-  fscanf(fptr, "%*s %d", &NETWORK_PROB);
+  fscanf(fptr, "%*s %lf", &QUIT_PROB);
+  fscanf(fptr, "%*s %lf", &NETWORK_PROB);
   fscanf(fptr, "%*s %d", &CPU_MIN);
   fscanf(fptr, "%*s %d", &CPU_MAX);
   fscanf(fptr, "%*s %d", &DISK1_MIN);
@@ -64,30 +74,57 @@ int main(int args, char *argv[]){
   fscanf(fptr, "%*s %d", &NETWORK_MIN);
   fscanf(fptr, "%*s %d", &NETWORK_MAX);
 
+  // printf("%d\n", FIN_TIME); 
+
   fclose(fptr);
 
   srand(SEED); // seed is from config file 
 
-  // int test = randomNumBetweenInterval(ARRIVE_MIN, ARRIVE_MAX);
-  // printf("%d\n", test); 
 
-  //Queue *cpuQueue;
-  //cpuQueue = initQueue();               // ***** cpuQueue->status 1 for BUSY 0 for IDLE *****
+  Queue *cpuQueue;
+  cpuQueue = initQueue();               // ***** cpuQueue->status 1 for BUSY 0 for IDLE *****
   //Queue *disk1Queue;
   //disk1Queue = initQueue(); 
   //Queue *disk2Queue;
   //disk2Queue = initQueue();
   //Queue *networkQueue;
   //networkQueue = initQueue();
+  Queue *pQueue;
+  pQueue = initQueue(); 
 
-  // Priority queue 
 
-  int clock = 0;  // will constantly change throughout 
-  int arrivalCounter = 0; // systemArrival counter for unique processID 
+  //int processIDCount = 0; // systemArrival counter for unique processID PUT IN handler 
 
-  // clock 0, add to the priorityQueue : systemArrival job1 and endSimulation time  
 
-  //startSimulation(INIT_TIME, &clock); 
+
+  // INIT 
+  handle_start_simulation(pQueue); // push START_SIM event onto pQueue, generate first job arrival 
+  handle_end_simulation(pQueue); // push END_SIM event onto pQueue (always lowest priority)
+  id_count = 0; 
+  // START_SIM -> PROCESS_ARRIVAL -> END_SIM ... start main loop 
+
+  int i = 0; 
+  // MAIN LOOP 
+  while(i<1){
+
+    // grab the top priority event 
+    struct event *e; 
+    e = malloc(sizeof(struct event)); 
+    e = popPQ(pQueue); 
+
+    switch(e->type)
+    {
+        case PROCESS_ARRIVAL:
+          handle_process_arrival(pQueue, cpuQueue, e); 
+          break; 
+        
+
+    }
+
+    ++i; 
+    
+  }
+
 
   // main loop to call handler func (use switch case to select correct event generator/handler)
     // clock set to next event time       clock is now: ... 
@@ -97,127 +134,106 @@ int main(int args, char *argv[]){
 
     // isBusy() change status 
 
-
-  // TESTING PQ 
-  Queue *q1; 
-  q1 = initQueue(); 
-
-  // testing 
-  struct event *s; 
-  s = malloc(sizeof(struct event)); 
-  s->type = START_SIM; 
-  s->time = 2; 
-
-  struct event *s2;
-  s2 = malloc(sizeof(struct event));
-  s2->type = CPU_START;
-  s2->time = 5; 
-  s2->id = 3; 
- 
-  struct event *s3;
-  s3 = malloc(sizeof(struct event));
-  s3->type = CPU_END;
-  s3->time = 15; 
-  s3->id = 4; 
   
+  // *********** main loop . pop event --> switch case correct event handler  
+  struct node *n; 
+  n = malloc(sizeof(struct node)); 
+  n = pQueue->front; 
 
-  struct event *s4;
-  s4 = malloc(sizeof(struct event));
-  s4->type = CPU_END;
-  s4->time = 1; 
-  s4->id = 1; 
-  // enqueue(q1, s);
-  // enqueue(q1, s2);
-
-  // //printf("%d", q1->rear->e->type);
-
-  // struct event *test;
-  // test = malloc(sizeof(struct event));  
-  // test = dequeue(q1); 
-
-  // printf("%d\n", test->type); 
-
-  // test = dequeue(q1); 
-  // printf("%d\n", test->type); 
-
-  // Queue *q5; 
-  // q5 = initQueue(); 
-
-  pushPQ(q1, s);
-  pushPQ(q1, s2);
-  pushPQ(q1, s3); 
-  pushPQ(q1, s4);
-
-  struct event *test = popPQ(q1);
-  // then free
-
-  printf("\n%d\n\n", test->time);
-  //printf("%d\n", q1->counter); 
-
-  //pushPQ(q1, s2); 
-
-  //printf("\n%d\n", q1->front->e->time);
-
-  struct node *n;
-
-  for(n = q1->front; n != 0; n = n->next) {
-      printf("%d ", n->e->time);
+  // print queue 
+  for(n = pQueue->front; n != 0; n = n->next) {
+      printf("%d ", n->e->type);
   }
 
+  //printf("%d", pQueue->front->e->type); 
 
   return 0; 
 }
 
-int randomNumBetweenInterval(int min, int max){
+int random_num_between_interval(int min, int max){
   return (min + rand() % (max-min+1)); // [min-max]
 }
 
 // handler func. for each event type (will actually change state of system)
-// START_SIM, SYSTEM_ARRIVAL, CPU_START, CPU_END, SYSTEM_EXIT, DISK1_ARRIVAL, DISK1_START, DISK1_END, DISK2_ARRIVAL DISK2_START, DISK2_END, NETWORK_ARRIVAL, 
-// NETWORK_START, NETWORK_END, END_SIM
+// START_SIM, SYSTEM_ARRIVAL, CPU_START, CPU_END, SYSTEM_EXIT, DISK1_ARRIVAL, DISK1_END, DISK2_ARRIVAL, DISK2_END, NETWORK_ARRIVAL, 
+// NETWORK_END, END_SIM
 
-void startSimulation(int INIT_TIME, int *clock, Queue *pQ){
-  int start = INIT_TIME;
-  *clock = start; // clock set to 0 
 
-  struct event *s; 
-  s = malloc(sizeof(struct event)); 
-  s->type = START_SIM; 
-  s->time = start; 
-  //s->id = 0;  // does need an ID ??? 
+void handle_start_simulation(Queue *pQ){
+  // will generate once 
+
+  struct event *e; 
+  e = malloc(sizeof(struct event)); 
+  e->type = START_SIM; 
+  e->time = INIT_TIME; 
+  e->id = 0; // doesn't need an id, prob. delete  
 
   // ADD TO PRIORITY QUEUE ... THIS IS FIRST EVENT 
-  pushPQ(pQ, s);
+  pushPQ(pQ, e);
+
+  struct event *new; 
+  new = malloc(sizeof(struct event)); 
+  new->type = PROCESS_ARRIVAL; 
+  new->time = id_count;  // 0  
+  new->id = 1;  
+
+  // ADD first job arrival (time 0) to queue, get the loop going ... 
+  pushPQ(pQ, new); 
 }
 
-// void endSimulation(int FIN_TIME, int *clock, Queue *pQ){
+void handle_end_simulation(Queue *pQ){
   
-//   struct event *e;
-//   e = malloc(sizeof(struct event)); 
-//   e->type = END_SIM; 
-//   e->time = FIN_TIME; 
+  struct event *e;
+  e = malloc(sizeof(struct event)); 
+  e->type = END_SIM; 
+  e->time = FIN_TIME; 
+  e->id = 0; // doesnt need an id, prob. delete
+
+  // ADD TO PRIORITY QUEUE ... SHOULD BE BOTTOM THROUGHOUT 
+  pushPQ(pQ, e); 
+}
+
+void handle_process_arrival(Queue *pQ, Queue *cpuQueue, struct event *old){
+  // will generate itself 
 
 
-//   // ADD TO PRIORITY QUEUE ... SHOULD BE BOTTOM THROUGHOUT 
+  if(cpuQueue->status == 1 || (cpuQueue->counter >0)){
+    // if CPU is busy, or the cpuQueue is non-empty, add process to queue  
 
-// }
+    enqueue(cpuQueue, old); 
+  }
+  else if(cpuQueue->status == 0 && cpuQueue->counter == 0){
+    // no cpuQueue required, generate NEXT event (PROCESS_ARRIVE_CPU)
 
-// void systemArrival(int ARRIVE_MIN, int ARRIVE_MAX, int *clock, int *counter){
-//   // generate a job arrival 
-//   // increment count for each arrival 
+    struct event *next; 
+    next = malloc(sizeof(struct event));
+    next->type = PROCESS_ARRIVE_CPU;
+    next->id = id_count; // id_count is still old event id
+    next->time = old->time; 
 
-// *counter = *counter+1; // increment 
-// int num = randomNumBetweenInterval(ARRIVE_MIN, ARRIVE_MAX); 
-// int nextEventTime = *clock + num; 
+    cpuQueue->status = 1; // CPU NOW BUSY 
+  }
 
-// struct event *sa; 
-// sa = malloc(sizeof(struct event)); 
-// sa->type = SYSTEM_ARRIVAL; 
-// sa->time = num; // [ARRIVE_MIN - ARRIVE_MAX]
-// sa->id = *counter; 
+  // NEW EVENT ARRIVAL 
+  ++id_count; // increment count, each func call, this is the processID 
+  int num = random_num_between_interval(ARRIVE_MIN, ARRIVE_MAX); 
 
-// // ADD TO PRIORITY QUEUE
+  struct event *new; 
+  new = malloc(sizeof(struct event)); 
+  new->type = PROCESS_ARRIVAL; 
+  new->id = id_count; 
+  new->time = old->time + num; // new event time 
 
-// }
+  pushPQ(pQ, new); // add to PQ 
+}
+
+void hand_process_arrive_cpu(Queue *pQ, struct event *old){
+  // will generate PROCESS_FINISH_CPU event 
+
+  int num = random_num_between_interval(CPU_MIN, CPU_MAX); 
+
+
+
+}
 
 
